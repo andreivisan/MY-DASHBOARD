c34a2e13903c9e3109d24b5c47710b330de27ba2
andreivisan <andrei.m.visan@gmail.com>
2023-02-07 20:07:06 +0100

    First commit for the authentication module

---
+++ authentication-module/src/test/java/io/programminglife/authentication/AuthenticationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.programminglife.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
